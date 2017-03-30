89f42260ac9d52a45b237267debab5d6e8808986
John-Lin <linton.tw@gmail.com>
2017-03-30 11:02:58 +0800

    updated example support python3

--- examples/simple_icmp.py
+++ examples/simple_icmp.py
@@ -31,11 +31,11 @@ def main():
 
         # Unpack the Ethernet frame (mac src/dst, ethertype)
         eth = dpkt.ethernet.Ethernet(buf)
-        print 'Ethernet Frame: ', mac_addr(eth.src), mac_addr(eth.dst), eth.type
+        print('Ethernet Frame: ', mac_addr(eth.src), mac_addr(eth.dst), eth.type)
 
         if eth.type != dpkt.ethernet.ETH_TYPE_IP:
-            print 'Non IP Packet type not supported %s\n' % eth.data.__class__.__name__
-
+            print('Non IP Packet type not supported %s\n' % eth.data.__class__.__name__)
+    
         # Now unpack the data within the Ethernet frame (the IP packet)
         # Pulling out src, dst, length, fragment info, TTL, and Protocol
         ip = eth.data
@@ -46,8 +46,8 @@ def main():
         fragment_offset = ip.off & dpkt.ip.IP_OFFMASK
 
         # Print out the info
-        print 'IP: %s -> %s   (len=%d ttl=%d DF=%d MF=%d offset=%d)\n' % \
-              (ip_to_str(ip.src), ip_to_str(ip.dst), ip.len, ip.ttl, do_not_fragment, more_fragments, fragment_offset)
+        print('IP: %s -> %s   (len=%d ttl=%d DF=%d MF=%d offset=%d)\n' % \
+              (ip_to_str(ip.src), ip_to_str(ip.dst), ip.len, ip.ttl, do_not_fragment, more_fragments, fragment_offset))
 
 if __name__ == '__main__':
     main()
